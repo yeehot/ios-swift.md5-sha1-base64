7dcef4393517939f8cdba8cfa1ef5e868654a533
Teng Wei <tengwei@tengmac.local>
2016-06-16 08:47:19 +0800

    Initial Commit

---
+++ MD5TESTTests/MD5TESTTests.swift
@@ -0,0 +1,36 @@
+//
+//  MD5TESTTests.swift
+//  MD5TESTTests
+//
+//  Created by Teng Wei on 16/6/16.
+//  Copyright © 2016年 yeehot org. All rights reserved.
+//
+
+import XCTest
+@testable import MD5TEST
+
+class MD5TESTTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
